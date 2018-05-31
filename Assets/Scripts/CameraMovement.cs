27c937a58dcdb35158dea616b9de714e217d9c67
MAROUANE AZZOUZ <azzouz.marouane123@gmail.com>
2018-05-31 10:37:01 +0200

    Premier commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform Player;
+    private Transform CameraPos;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start ()
+    {
+        CameraPos = GetComponent<Transform>();
+        offset = CameraPos.position - Player.position ;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CameraPos.position = Player.position + offset;
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    private float moveVertical;
+    private float moveHorizontal;
+    private Vector3 movement;
+    public float speed = 1;
+
+    private void Start()
+    {
+        
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * Time.deltaTime * speed) ;
+	}
+}
